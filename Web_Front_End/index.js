a7aba6b3115319614fa00236804aa97b863807be
ThaiNhatThu <21522648@gm.uit.edu.vn>
2024-04-27 13:11:56 +0700

    update website

--- Web_Front_End/index.js
+++ Web_Front_End/index.js
@@ -5,9 +5,19 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/Dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/Profile", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "users-profile.html"));
+});
+
+app.get("/Login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "pages-login.html"));
+});
+
+
 
 // Route để phản hồi với tệp JavaScript trong thư mục public/js/main
 app.get('/js/main/main.js', (req, res) => {
