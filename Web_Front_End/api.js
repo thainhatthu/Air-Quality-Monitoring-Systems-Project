e958a958d8ddeca36a8e2bff3e69864767aec62c
@hieutran03 <hieumdg@gmail.com>
2024-05-02 13:18:01 +0700

    AJAX and device auth

--- Web_Front_End/api.js
+++ Web_Front_End/api.js
@@ -1,20 +1,26 @@
 const firebaseHandler = require('./firebaseHandle');
-const { getDatabase, set, ref } = require ("firebase/database");
-
-
+// const { getDatabase, set, ref } = require ("firebase/database");
+const { deviceAuth } = require('./auth');
+Number.prototype.round = function(p) {
+  p = p || 10;
+  return parseFloat( this.toFixed(p) );
+};
 
 module.exports =(app)=>{
   
-  app.post('/api/data', async(req,res)=>{
+  app.post('/api/data', deviceAuth, async(req,res)=>{
     const data = req.body;
-    console.log(data);
+    const user_id = req.get('user_id');
     try{
-      data.temperature = parseFloat(data.temperature);
-      data.humidity = parseFloat(data.humidity);
-      data.dust = parseFloat(data.dust);
-      data.ppm = parseFloat(data.ppm);
-      await firebaseHandler.writeData(data);
-    }catch{
+      data.temperature = parseFloat(data.temperature).round(2);
+      data.humidity = parseFloat(data.humidity).round(2);
+      data.dust = parseFloat(data.dust).round(2);
+      data.ppm = parseFloat(data.ppm).round(2);
+      console.log(data);
+
+      await firebaseHandler.writeData(data, user_id);
+    }catch(e){
+      console.log("Error: ", e);
       res.status(400).json({
         status: 'error',
         message: 'Invalid data'
