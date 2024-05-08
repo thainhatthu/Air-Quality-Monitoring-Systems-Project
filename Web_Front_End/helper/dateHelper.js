1b9b328ae7cd1225c1d062ddf6720dee300134ac
@hieutran03 <hieumdg@gmail.com>
2024-05-08 23:01:27 +0700

    test

--- Web_Front_End/helper/dateHelper.js
+++ Web_Front_End/helper/dateHelper.js
@@ -2,10 +2,6 @@ const formatDate = (date) => {
   let month = date.getMonth()+1;
   let day = date.getDate();
   let year = date.getFullYear();
-  // let hour = date.getHours();
-  // let minute = date.getMinutes();
-  // let second = date.getSeconds();
-  // return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
   return `${day}-${month}-${year}`;
 }
 function getRndInteger(min, max) {
